7a1cb19f3873fd4bf10f87b15acad9fd5ea056e7
Piotr Lewandowski <grovman@gmail.com>
2013-11-30 21:20:12 +0100

    countingSort: sort by first letter

--- countingSort_lab04/countingSort.c
+++ countingSort_lab04/countingSort.c
@@ -7,73 +7,79 @@
 #define MN 10 // maks il napisow
 
 
-char **A,**B,**T;
+#define DEBUG(var, type) \
+  printf(" %i: " #var " == " #type " \n", __LINE__, var)
 
-void drukuj(char **A,int n){
+
+char **A,**B,**TMP;
+
+void drukuj(char **A, int n) {
   // drukuje  drugie litery napisow i napisy  
   int i;
   for (i=0;i<n;i++){
-    printf("%c %s",*(A[i]+1),A[i]);  
+    printf(" %s",A[i]);  
+    // printf("%c %s",*(A[i]+1),A[i]);  
 //czyli   printf("%c %s",*(*(A+i)+1),*(A+i));  
     printf("\n");
   }
 }
 
-void czytaj(char **A, int ilosc){
- char slowo[MDN];
- int i=0;
- for (i=0;i<ilosc; i++){
+void czytaj(char **A, int ilosc) {
+  char slowo[MDN];
+  int i = 0;
+  for(i = 0; i < ilosc; i++) {
     scanf("%s",slowo);
     A[i] = (char*) malloc(sizeof(char)*MDN);
-    strcpy(A[i],slowo);
- }
+    strcpy(A[i], slowo);
+  }
 }
 
-void countingSort(char *input, char *output, int k) {
+void countingSort(char **input, char **output, int k) {
   // k - najwiekszy element w tablicy input
   
   int i, j;
-  int tmp[k+1]; // C
+  char count[MDN+1]; // tablica pomocnicza, zmienia swoja zawartosc po kazdej petli
 
   // Zerujemy tablice zliczajaca ilosc powtorzen
   // wszystkich elementow tablicy input
 
-  for(i = 0; i <= k; i++)
-    tmp[i] = 0;
+  for(i = 0; i <= MDN; i++)
+    count[i] = 0;
 
   // zliczanie powtorzen kazdej litery z input[j]
   // kiedy 'a' wystepuje dwa razy, to:
-  // tmp['a'] == 2 || tmp[65] == 2
+  // count['a'] == 2 || count[65] == 2
 
-  for(j = 0; j <= MDN; j++)
-    tmp[ input[j] ] += 1;
+  for(j = 0; j < k; j++) {
+    count[ (int) input[j][0] ] += 1;
+  }
 
-  // tmp[i] bedzie przechowywac ilosc elementow
-  // mniejszych lub rownych od tmp[i]
+  // count[i] bedzie przechowywac ilosc elementow
+  // mniejszych lub rownych od count[i]
 
-  for(i = 1; i <= k; i++)
-    tmp[i] += tmp[i-1];
-
-  // 
+  for(i = 1; i <= MDN; i++) {
+    count[i] += count[i-1];
+    // DEBUG( count[i], %i );
+  }
   
-  for(j = MDN; j >= 0; j--) {
+  for(j = k-1; j >= 0; j--) {
     // T=A; A=B; B=T; // zamiana wskaznikow do tablic
-    output[ tmp [ input [j] ]-1 ] = input[j];
-    tmp[ input[j] ] -= 1;
+    output[ (int) count[ (int) input[j][0] ]-1 ] = input[j];
+    count[ (int) input[j][0] ] -= 1;
+    // DEBUG( output[ (int) count[ (int) input[j][0] ] ], %i);
+    // DEBUG( input[j][0], %i);
   }
 }
 
 int main() {
-  int n=2;
+  int n=3;
   A=(char**) malloc(n*sizeof(char*));
   B=(char**) malloc(n*sizeof(char*));
   printf("tablica A: \n"); czytaj(A,n); 
-  printf("tablica B: \n"); czytaj(B,n); 
-  printf("tablica A: \n"); drukuj(A,n);
-  printf("tablica B: \n"); drukuj(B,n);
-  printf("zamiana \n"); T=A; A=B; B=T; 
-  printf("tablica A: \n"); drukuj(A,n);
-  printf("tablica B: \n"); drukuj(B,n);
+
+  countingSort(A, B, n);
+
+  printf("sorted: \n"); drukuj(B,n);
 
   return 0;
 }
