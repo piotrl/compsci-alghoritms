463515cb541ffb8517e32bb41ca1985c5bcb314d
Piotr Lewandowski <grovman@gmail.com>
2014-03-08 11:42:17 +0100

    Printing all longest substrings

--- dynamicProg_lab09/longestSubString.c
+++ dynamicProg_lab09/longestSubString.c
@@ -20,10 +20,10 @@
 #define UP 1
 #define LEFT 2
 
-void printMatrix(int ilen, int jlen, int matrix[ilen][jlen])
+void printMatrix(int ilen, int jlen, int matrix[][100], char id)
 {	// ilen - rows
 	// jlen 
-	printf("\n id  ");
+	printf("\n %c   ", id);
 
 	for (int i = 0; i < jlen; ++i)
 		printf("%i ", i);
@@ -44,16 +44,24 @@ void printMatrix(int ilen, int jlen, int matrix[ilen][jlen])
 	}
 }
 
+	int c[100][100],
+		b[100][100];
+
 int lcsLenght(	int ylen, int xlen, // lengths of strings
-				char* y, char* x, 	// strings
-				int c[ylen+1][xlen+1], int b[ylen][xlen]	// arrays with NWP
+				char* y, char* x 	// strings
 			 )
 {	// x and y - strings to compare
 	
 	for (int i = 0; i <= ylen; ++i)
+	{
 		c[i][0] = 0;
+		b[i][0] = 8;
+	}
 	for (int i = 0; i <= xlen; ++i)
+	{
 		c[0][i] = 0;
+		b[0][i] = 8;
+	}
 
 	// filling arrays 
 	for (int i = 1; i <= ylen; ++i)
@@ -63,17 +71,17 @@ int lcsLenght(	int ylen, int xlen, // lengths of strings
 			if (x[j-1] == y[i-1])
 			{
 				c[i][j] = c[i-1][j-1]+1;
-				b[i-1][j-1] = UPLEFT;		// get from up left
+				b[i][j] = UPLEFT;		// get from up left
 			}
 			else if (c[i-1][j] >= c[i][j-1])
 			{
 				c[i][j] = c[i-1][j];
-				b[i-1][j-1] = UP;			// get from up
+				b[i][j] = UP;			// get from up
 			}
 			else
 			{
 				c[i][j] = c[i][j-1];
-				b[i-1][j-1] = LEFT;			// get from left			
+				b[i][j] = LEFT;			// get from left			
 			}
 		}
 	}
@@ -82,7 +90,6 @@ int lcsLenght(	int ylen, int xlen, // lengths of strings
 }
 
 void lcsPrint( 	int i, int j, // i == ylen, j == xlen
-				int b[i][j],  // i,j == initial position of path 
 				char* x
 			 )
 {
@@ -93,17 +100,19 @@ void lcsPrint( 	int i, int j, // i == ylen, j == xlen
 
 	if (b[i][j] == UPLEFT)			
 	{
-		printf(" i == %i, j == %i \n", i, j);
-		lcsPrint( i-1, j-1, b, x);	// go up left
-		printf("%c", x[j]);
+		// printf("UPLEFT \ti == %i, j == %i, b[%i][%i] == %i \n", i, j, i, j, b[i][j]);
+		lcsPrint( i-1, j-1, x);	// go up left
+		printf("%c", x[j-1]);
 	} 
 	else if (b[i][j] == UP) 	
 	{
-		lcsPrint(i-1, j, b, x);		// go up
+		// printf("UP: \ti == %i, j == %i, b[%i][%i] == %i \n", i, j, i, j, b[i][j]);
+		lcsPrint(i-1, j, x);		// go up
 	} 
 	else
 	{
-		lcsPrint(i, j-1, b, x); 	// go left
+		// printf("LEFT: \ti == %i, j == %i, b[%i][%i] == %i \n", i, j, i, j, b[i][j]);
+		lcsPrint(i, j-1, x); 	// go left
 	}
 }
 
@@ -128,33 +137,30 @@ int main(int argc, char const *argv[])
 	strcpy(x, argv[1]);
 	strcpy(y, argv[2]);
 
-
-	int c[ylen+1][xlen+1],
-		b[ylen][xlen];
 	
-	int longestSubString = lcsLenght(ylen, xlen, y, x, c, b);
+	int longestSubString = lcsLenght(ylen, xlen, y, x);
 
 	printf(" %s\n %s\n\n", x, y );
 	printf("-----------------------------------\n");
 
-	printMatrix(ylen+1, xlen+1, c);
+	printMatrix(ylen+1, xlen+1, c, 'c');
 
 	putchar('\n');
 	printf("-----------------------------------\n");
 	printf(" 0 - UPLEFT; 1 - UP; 2 - LEFT\n");
 
-	printMatrix(ylen, xlen, b);
+	printMatrix(ylen+1, xlen+1, b, 'b');
 
 	putchar('\n');
 	printf("-----------------------------------\n");
 
 	printf(" length of longest substring: %i\n", longestSubString);
-	// while(xlen >= 0 && c[ylen][xlen] == longestSubString)
-	// {
-		lcsPrint(ylen-1, xlen-1, b, x);
-	// 	putchar('\n');
-	// 	xlen--;
-	// }
+	while(xlen >= 0 && c[ylen][xlen] == longestSubString)
+	{
+		lcsPrint(ylen, xlen, x);
+		putchar('\n');
+		xlen--;
+	}
 	
 	putchar('\n');
 	putchar('\n');
