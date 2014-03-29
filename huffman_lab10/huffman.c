86ce43a02e6ea9951191194dde799aee02bea619
Piotr Lewandowski <grovman@gmail.com>
2014-03-29 10:10:23 +0100

    Huffman code: calc frequency and init leafs

---
+++ huffman_lab10/huffman.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+	char key;
+	int value;
+	struct node *parent;
+	struct node *left;
+	struct node *right;
+} typedef Elem;
+
+Elem* newElement();
+void transplant(Elem* root, Elem* u, Elem* v);
+
+/**
+ * public methods - used in main
+ * ************************************************ */
+
+Elem* initLeaf(int key, int frequency)
+{
+	Elem* node 	= newElement();
+	node->key 	= key;
+	node->value = frequency;
+	node->parent= NULL;
+	node->left 	= NULL;
+	node->right	= NULL;
+
+	return node;
+}
+
+void treePrintInOrder(Elem* node) 
+{
+	if (node != NULL)
+	{
+		treePrintInOrder(node->left);
+		printf("%i ", node->key);
+		treePrintInOrder(node->right);
+	}
+}
+
+Elem* treeSearch(Elem* node, int value) 
+{
+	if (node == NULL || value == node->key)
+	{
+		return node;
+	}
+	else if (value < node->key)
+	{
+		return treeSearch(node->left, value);
+	}
+	else
+	{
+		return treeSearch(node->right, value);
+	}
+}
+
+Elem* treeInsert(Elem* Tree, int value) 
+{
+	Elem* newEl = newElement();
+	newEl->key = value;
+
+	Elem* newElParent = NULL;
+	Elem* node = Tree;		// first element of Tree is root
+
+	while (node != NULL)
+	{	// find place for new element 
+		newElParent = node;
+		node = (newEl->key < node->key) ? node->left : node-> right;
+	}
+
+	newEl->parent = newElParent;
+
+	if (newElParent == NULL)
+	{
+		Tree = newEl;	// Tree is empty
+	} 
+	else if (newEl->key < newElParent->key)
+	{
+		newElParent->left = newEl;
+	}
+	else
+	{
+		newElParent->right = newEl;
+	}
+
+	return newEl;
+}
+
+int main(int argc, char const *argv[])
+{
+
+	int every_leters[255] = {0};
+
+	char c = 0;
+	int i, j, ls = 0;
+	while (c != EOF)
+	{
+		c = getchar();
+		every_leters[ (int) c]++;
+	}
+
+	for(i = 0; i < 255; i++)
+	{
+		if(every_leters[i] != 0)
+			ls++;
+	}
+
+	Elem *letters[ls];
+
+	for(i = 0, j = 0; i < 255 && j < ls; i++)
+	{
+		if(every_leters[i] != 0)
+		{
+
+			letters[j++] = initLeaf(i, every_leters[i]);
+		}
+	}
+
+	for (i = 0; i < ls; i++)
+	{
+		printf("%c: %i\n", letters[i]->key, letters[i]->value );
+	}
+
+	putchar('\n');
+	return 0;
+}
+
+/**
+ * private methods
+ * ************************************************ */
+
+Elem* newElement() 
+{
+	return (Elem*) malloc( sizeof(Elem) );
+}
+
+Elem* treeMinimum(Elem* root) {
+	while(root->right != NULL)
+	{
+		root = root->right;
+	}
+
+	return root;
+}
+
+void transplant(Elem* root, Elem* u, Elem* v)
+{
+	if (u->parent == NULL)
+	{
+		root = v;
+	}
+	else if (u == u->parent->left)
+	{
+		u->parent->left = v;
+	}
+	else
+	{
+		u->parent->right = v;
+	}
+
+	if (v != NULL)
+	{
+		v->parent = u->parent;
+	}
+}
