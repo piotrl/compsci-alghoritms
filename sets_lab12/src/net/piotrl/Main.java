8c6e3faa0e9d4364ab0b690cb146e14f4742e364
Piotr Lewandowski <grovman@gmail.com>
2014-05-05 21:04:06 +0200

    Kruskal alghoritm

--- sets_lab12/src/net/piotrl/Main.java
+++ sets_lab12/src/net/piotrl/Main.java
@@ -1,33 +1,85 @@
 package net.piotrl;
 
+import javax.sound.midi.Soundbank;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-        Set[] Z = new Set[10];
+        // -------------------------------
+        // G = (V, E, w)
 
-        Z[0] = Set.make(0);
-        Z[1] = Set.make(1);
-        Z[2] = Set.make(2);
-        Z[3] = Set.make(3);
-        Z[4] = Set.make(4);
-        Z[5] = Set.make(5);
-        Z[6] = Set.make(6);
-        Z[7] = Set.make(7);
-        Z[8] = Set.make(8);
-        Z[9] = Set.make(9);
+        List<int[]> edges = new ArrayList<int[]>();
+        // v = edge[0]
+        // u = edge[1]
+        // weight = edge[2]
 
-        Z[0].union(Z[1]);
-        Z[0].union(Z[3]);
-        Z[0].union(Z[3]);
-        Z[2].union(Z[3]);
-        Z[4].union(Z[5]);
-        Z[6].union(Z[7]);
+        edges.add(new int[] {1, 2, 1});
+        edges.add(new int[] {1, 3, 20});
+        edges.add(new int[] {2, 3, 0});
+        edges.add(new int[] {4, 3, 3});
+        edges.add(new int[] {4, 1, 2});
+        edges.add(new int[] {4, 2, 5});
+        edges.add(new int[] {5, 2, 5});
 
 
-        for (Set set : Z) {
-            System.out.println(set);
+        List<int[]> tree = kruskal(edges);
+
+        int i = 0;
+        for (int[] ints : tree) {
+            i++;
+            System.out.println(ints[0] + " - " + ints[1]);
         }
+
+
+    }
+
+    private static List<int[]> kruskal(List<int[]> edges) {
+        List<int[]> A = new ArrayList<int[]>();
+
+        Map<Integer, Set> V = createGraphValues(edges);
+
+        // sort edges
+        Collections.sort(edges, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] e1, int[] e2) {
+                return e1[2] - e2[2];   // e1[2] - weight of e1
+            }
+        });
+
+        for (int[] edge : edges) {
+            Set v = V.get(edge[0]);
+            Set u = V.get(edge[1]);
+
+            if (Set.find(u) != Set.find(v) ) {
+                A.add(edge);
+                u.union(v);
+            }
+        }
+
+        System.out.println("|V| = " + V.values().size());
+        System.out.println("|A| = " + A.size());
+
+        return A;
+    }
+
+    private static Map<Integer, Set> createGraphValues(List<int[]> E) {
+        Map<Integer, Set> V = new HashMap<Integer, Set>();
+
+        for (int[] edge : E) {
+            int v = edge[0];
+            int u = edge[1];
+
+            if (!V.containsKey(v)) {
+                V.put(v, Set.make(v));
+            }
+
+            if (!V.containsKey(u)) {
+                V.put(u, Set.make(u));
+            }
+        }
+
+        return V;
     }
 }
